aedf094928c38392d06e0ab903841d6732ffe292
Mani Kandan <mkandan@asu.edu>
2019-05-02 13:10:37 -0700

    Initial commit

---
+++ data_cleaner.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+
+# Read in data into a dataframe
+data = pd.read_csv('/Users/mani/PycharmProjects/ny 17 energy predictor/nyc_benchmarking_disclosure_data_reported_in_2017.xlsb')
+
+# Display top of dataframe
+data.head()
